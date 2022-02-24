7a7f2d44ada5b00454565540fbca028058f21e89
Maria Trofimova <maria.trofimova@oxagile.com>
2022-02-24 15:51:32 +0300

    getWeather async method

--- commands/weather.js
+++ commands/weather.js
@@ -1,39 +1,27 @@
 const readline = require("../readline");
 const fetch = require("node-fetch");
 
-const getWeather=async function getWeather(callback) {
+const getWeather = async function getWeather(callback) {
   readline.question(
     "Введи название города в формате - Moscow,ru: ",
-    (cityName) => {
-      fetch(
+    async (cityName) => {
+      let response = await fetch(
         `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=31437cba04d38b2ed41b44a1882df5f7&lang=ru`
-      )
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          const temperature = data.main.temp;
-          const skyDescription = data.weather[0].description;
-          const minTemperature = data.main.temp_min;
-          const maxTemperature = data.main.temp_max;
-          console.log(skyDescription);
-          console.log("Текущая температура: ", temperature, "*C");
-          console.log(
-            "Минимальная температура сегодня: ",
-            minTemperature,
-            "*C"
-          );
-          console.log(
-            "Максимальная температура сегодня: ",
-            maxTemperature,
-            "*C"
-          );
-          callback();
-        })
-        .catch((_error) => {
-          console.log("Такой город не найден!");
-          callback();
-        });
+      );
+      let data = await response.json();
+      if (data.cod === "404" || !cityName) {
+        console.log("Упс! Такой город не найден!");
+      } else {
+        let skyDescription = await data.weather[0].description;
+        console.log(skyDescription);
+        let temperature = await data.main.temp;
+        console.log("Текущая температура: ", temperature, "*C");
+        let minTemperature = await data.main.temp_min;
+        console.log("Минимальная температура сегодня: ", minTemperature, "*C");
+        let maxTemperature = await data.main.temp_max;
+        console.log("Максимальная температура сегодня: ", maxTemperature, "*C");
+        callback();
+      }
     }
   );
 };
