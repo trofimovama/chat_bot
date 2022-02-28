a15953f58ad8c95a87072c62b516276d2bc7dc25
Maria Trofimova <maria.trofimova@oxagile.com>
2022-02-28 10:57:56 +0300

    improve weather app option

--- commands/weather.js
+++ commands/weather.js
@@ -5,16 +5,16 @@ const getWeather = async function getWeather(callback) {
   readline.question(
     "Введи название города в формате - Moscow,ru: ",
     async (cityName) => {
-      let response = await fetch(
+      const response = await fetch(
         `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=31437cba04d38b2ed41b44a1882df5f7&lang=ru`
       );
-      let data = await response.json();
+      const data = await response.json();
       if (data.cod === "404" || !cityName) {
         console.log("Упс! Такой город не найден!");
       } else {
         let skyDescription = await data.weather[0].description;
         console.log(skyDescription);
-        let temperature = await data.main.temp;
+        const temperature = await data.main.temp;
         console.log("Текущая температура: ", temperature, "*C");
         let minTemperature = await data.main.temp_min;
         console.log("Минимальная температура сегодня: ", minTemperature, "*C");
