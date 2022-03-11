9dce7a0ad1245ab7696e0acd17463c24c752309e
Maria Trofimova <maria.trofimova@oxagile.com>
2022-03-11 12:30:02 +0300

    array option fixed

--- commands/sortArray.js
+++ commands/sortArray.js
@@ -5,8 +5,21 @@ const sortArray = (callback) => {
     const newSet = new Set([userInput]);
     console.log("Вы ввели: ", newSet);
     let userArray = Object.values(userInput.split(","));
+    let sortedArray = [];
+    let length = userArray.length ;
+
     userArray.sort();
-    console.log("Отсортировано успешно! ", userArray);
+
+    let temporary;
+
+    for (let i = 0; i < length; i++) {
+      if(userArray[i] !== temporary) {
+        sortedArray.push(userArray[i]);
+        temporary = userArray[i];
+      }
+    }
+
+    console.log("Отсортировано успешно! ", sortedArray);
     callback();
   });
 };
