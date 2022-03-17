e47e7e8a231485c57c9c61d44f7f2daf1df856fe
Maria Trofimova <maria.trofimova@oxagile.com>
2022-03-17 14:31:40 +0300

    fix functions named params

--- destruct.js
+++ destruct.js
@@ -1,26 +1,33 @@
 const multipleArgumentsFunc = (...arguments) => {
-    arguments.forEach(element => {
-        console.log(`${element} is ${typeof(element)}`);
-    })
+     arguments.forEach(element => {
+          console.log(`${element} is ${typeof(element)}`);
+     }) 
 };
 
-const namedParametersFunc = (a, b, c) => {
-    const result = [a, b, c];
-    result.forEach(elem => {
-        console.log(`${elem} is ${typeof(elem)}`)
-    })
-};
+const unnamedParametersFunc = (objectOption) => {
+    const  a = objectOption.a;
+    const b = objectOption.b;
+    const c = objectOption.c;
+          for (let key in objectOption) {
+            if(objectOption.hasOwnProperty(key)){
+            console.log(`${objectOption[key]} is ${typeof(objectOption[key])}`)
+          }
+    }
+}; 
 
 const unnamedTwoParamsFunc = (argOne, argTwo, ...arguments) => {
    console.log(argOne,argTwo);
     multipleArgumentsFunc(...arguments);
 };
 
-const unnamedThreeParamsFunc = (argOne, argTwo, argThree) => {
-    namedParametersFunc(argOne, argTwo, argThree);
+const unnamedThreeParamsFunc = (values) => {
+    const optionOne = values.optionOne;
+    const optionTwo = values.optionTwo;
+    const optionThree = values.optionThree;
+    unnamedParametersFunc(values);
 }
 
 multipleArgumentsFunc('Boom', 2, [1,2,3]);
-namedParametersFunc(3, 'Masha', true);
+unnamedParametersFunc({a: 3, b:'Masha', c: true});
 unnamedTwoParamsFunc('And', 'still', 'I', 'rise');
 unnamedThreeParamsFunc(1, '100', false);
