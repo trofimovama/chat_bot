f08594ee5bcf9a4c0342cb296ba0f43904b57cd7
Maria Trofimova <maria.trofimova@oxagile.com>
2022-04-11 10:56:48 +0300

    try! to implement weather npm

--- commands/weather.js
+++ commands/weather.js
@@ -1,47 +1,7 @@
-const readline = require("../readline");
-const fetch = require("node-fetch");
 require("dotenv").config();
+const getWeather = require("get_weather_package");
+// const { getWeather } = require("./commands");
 
-const getWeather = async (callback) => {
-  readline.question(
-    "Введи название города в формате - Moscow,ru: ",
-    async (cityName) => {
-      const response = await fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${process.env.API_KEY}&lang=ru`
-      );
-      const data = await response.json();
-      if (data.cod === "404" || !cityName) {
-        console.log("Упс! Такой город не найден!");
-      } else {
-        const skyDescription = data.weather[0].description;
-        console.log(skyDescription);
-        const temperature = data.main.temp;
-        const minTemperature = data.main.temp_min;
-        const maxTemperature = data.main.temp_max;
+console.log(getWeather('Minsk'));
 
-        const weatherData = [
-          {
-            title: "Текущая температура: ",
-            value: temperature,
-          },
-          {
-            title: "Минимальная температура сегодня: ",
-            value: minTemperature,
-          },
-          {
-            title: "Максимальная температура сегодня: ",
-            value: maxTemperature,
-          },
-        ];
-        
-        weatherData.forEach(element => {
-          console.log(element.title, element.value, '*C');
-        })
 
-        callback();
-      }
-    }
-  );
-};
-
-module.exports = getWeather;
